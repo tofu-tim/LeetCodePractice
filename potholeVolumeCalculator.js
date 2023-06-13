da30beb4b4f7e77c8c01ef9a7eb35978f74bb493
Timothy Wilger <47797775+tofu-tim@users.noreply.github.com>
2023-06-13 08:34:34 -0500

    Add files via upload

---
+++ potholeVolumeCalculator.js
@@ -0,0 +1,12 @@
+/*
+The goal of this assignment is to find the amount of negative space that exists within a histogram.
+the negative space behaves as water would.
+If column A = 5, B = 3, C = 5:
+Program should return 2.
+If Column A = 5, B = 3, C = 4:
+Program should return 1.
+*/
+
+function potholeVolume(array) {
+
+}
