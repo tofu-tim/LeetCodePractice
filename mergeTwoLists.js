99fe6d045cd142296a3d1f08ce2a9778654cb055
Timothy Wilger <47797775+tofu-tim@users.noreply.github.com>
2023-07-21 23:40:35 -0500

    Add files via upload

--- mergeTwoLists.js
+++ mergeTwoLists.js
@@ -1,30 +1,30 @@
 function mergeTwoLists(list1, list2) {
-    megaList = [];
-    for (var x = 0; x < list1.length; x++) {
-        megaList.pop(list1[x]);
-    }
-    for (var x = 0; x < list2.length; x++) {
-        megaList.pop(list2[x]);
-    }
+    // Merge the two lists
+    const megaList = [...list1, ...list2];
 
-    function bubbleSort(megaList) {
+    // Define the bubbleSort function
+    function bubbleSort(arr) {
         let swapped;
         do {
             swapped = false;
-            for (let i = 0; i < megaList.length - 1; i++) {
-                if (megaList[i] > megaList[i + 1]) {
-                    let temp = megaList[i];
-                    megaList[i] = megaList[i + 1];
-                    megaList[i + 1] = temp;
+            for (let i = 0; i < arr.length - 1; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    let temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
                     swapped = true;
                 }
             }
         } while (swapped);
 
-        return megaList;
+        return arr;
     }
+
+    // Sort the merged list using bubbleSort
+    const sortedList = bubbleSort(megaList);
+
+    return sortedList;
 }
 
-test1 = mergeTwoLists([1, 23, 4, 5, 6, 7, 8, 9], [9, 8, 7, 6, 5, 4, 3, 2, 1]);
-
-console.log(test1)
+test1 = mergeTwoLists([1, 2, 3, 4, 5, 6, 7, 8, 9], [9, 8, 7, 6, 5, 4, 3, 2, 1]);
+console.log(test1);
