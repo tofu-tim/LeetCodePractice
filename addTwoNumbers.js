ea0a453f4ba77301b02d3c3b7c1deef26c9deb7d
Timothy Wilger <47797775+tofu-tim@users.noreply.github.com>
2023-07-26 23:15:40 -0500

    Add files via upload

--- addTwoNumbers.js
+++ addTwoNumbers.js
@@ -1,25 +1,32 @@
 function addTwoNumbers(list1, list2) {
-    let newList = []
-    let newInt = 0
-    list1Reversed = list1.reverse();
-    list2Reversed = list2.reverse();
-    for (var i = 0; i < list1Reversed.length; i++) {
-        list1Integer = 0;
-        list1Integer += (list1Reversed[i] * (10 ^ i));
-        return list1Integer
+    let newList = [];
+    let newInt = 0;
+
+    // Create copies of the input lists to avoid modifying the original lists
+    let list1Reversed = list1.slice().reverse();
+    let list2Reversed = list2.slice().reverse();
+
+    console.log("list1Reversed = " + list1Reversed);
+    console.log("list2Reversed = " + list2Reversed);
+
+    let list1Integer = 0;
+    for (let i = 0; i < list1Reversed.length; i++) {
+        list1Integer += list1Reversed[i] * (10 ** i);
     }
 
-    for (var i = 0; i < list2Reversed.length; i++) {
-        list2Integer = 0;
-        list2Integer += (list2Reversed[i] * (10 ^ i));
-        return list2Integer
+    console.log("list1Interger = " + list1Integer);
+
+    let list2Integer = 0;
+    for (let i = 0; i < list2Reversed.length; i++) {
+        list2Integer += list2Reversed[i] * (10 ** i);
     }
 
-    newInt == (list1Integer + list2Integer)
+    console.log("list2Interger = " + list2Integer);
 
-    return newInt
+    newInt = list1Integer + list2Integer;
+
+    return newInt;
 }
 
-let test1 = addTwoNumbers([1, 2, 3, 4, 5], [1, 2, 3, 4, 5])
-
-console, log(test1);
+let test1 = addTwoNumbers([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]);
+console.log(test1);
